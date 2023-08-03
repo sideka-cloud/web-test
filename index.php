bd927ad0ac29da51e2a1069376c5aab8ae848cbb
sideka-cloud <89623723+sideka-cloud@users.noreply.github.com>
2023-08-03 11:32:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,21 @@
   // Get server hostname and IP address
   $hostname = gethostname();
   $ip = $_SERVER['SERVER_ADDR'];
+  // Check PHP version
+  $phpversion = phpversion();
   ?>
   <header>
-    <h1>Welcome to my page!</h1>
+    <h1>Welcome To My Page!</h1>
+    <h3>image v2</h3>
   </header>
   <main>
     <img src="img/cat1.png" alt="cat" />
     <p>Server hostname: <?php echo $hostname; ?></p>
     <p>Server IP address: <?php echo $ip; ?></p>
+    <p>PHP version: <?php echo $phpversion; ?></p>
   </main>
+  <footer>
+    <p>&copy; 2023 sys-ops.id</p>
+  </footer>
 </body>
 </html>
