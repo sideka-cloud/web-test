78967ae618287cf3dc9d0d1757f3e07fb1ea1bcf
sideka-cloud <89623723+sideka-cloud@users.noreply.github.com>
2024-02-13 21:57:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,22 +8,22 @@
   <?php
   // Get server hostname and IP address
   $hostname = gethostname();
-  $ip = $_SERVER['SERVER_ADDR'];
+  $ip = ($_SERVER['SERVER_ADDR'] === '127.0.0.1' || $_SERVER['SERVER_ADDR'] === '::1') ? $_SERVER['REMOTE_ADDR'] : $_SERVER['SERVER_ADDR'];
   // Check PHP version
   $phpversion = phpversion();
   ?>
   <header>
-    <h1>Welcome To My Page!</h1>
-    <h3>image v3 - Aug 2023</h3>
+    <h1>Welcome To My Site!</h1>
+    <h3>image v4 - Feb 2024</h3>
   </header>
   <main>
-    <img src="img/cat2.png" alt="cat" />
+    <img src="img/cat4.png" alt="cat" />
     <p>Server hostname: <?php echo $hostname; ?></p>
     <p>Server IP address: <?php echo $ip; ?></p>
     <p>PHP version: <?php echo $phpversion; ?></p>
   </main>
   <footer>
-    <p>&copy; 2023 sys-ops.id</p>
+    <p>&copy; 2024 sys-ops.id</p>
   </footer>
 </body>
 </html>
